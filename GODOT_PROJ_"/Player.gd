fbd6309d12c884fc575d0c7c8d0740d37448c3b8
Martim Rosa Monis <martimrosam@tecnico.ulisboa.pt>
2023-03-11 10:08:42 +0000

    epic last moment

--- GODOT_PROJ_"/Player.gd
+++ GODOT_PROJ_"/Player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 var health = 3
 @onready var animationPlayer = $PlayerAP
+@onready var timer = $Timer
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -11,10 +12,17 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * SPEED, SPEED/2)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if (Input.is_action_just_pressed("ui_up") and timer.is_stopped()):
+		print("BOOST!")
+		velocity.x += 1000
+		timer.set_wait_time(5)
+		timer.start()
 		
 	animate(velocity)
 
@@ -33,4 +41,8 @@ func animate(velocity):
 func _on_area_2d_body_entered(body):
 	health -= 1
 	if (health == 0):
-		get_tree().change_scene_to_file(node_2d)
+		get_tree().change_scene_to_file("res://node_2d.tscn")
+
+
+func _on_timer_timeout():
+	timer.stop()
--- GODOT_PROJ_"/thunder_script.gd
+++ GODOT_PROJ_"/thunder_script.gd
@@ -1,11 +1,23 @@
 extends Area2D
 
 @onready var animationPlayer = $AnimationPlayer
+@onready var timer = $Timer
+@onready var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	animationPlayer.play("thunderclap")
+	rng.randomize()
+	timer.set_wait_time(rng.randf_range(1, 6))
+	timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_timer_timeout():
+	animationPlayer.play("thunderclap")
+	timer.set_wait_time(rng.randf_range(1.5, 6))
+	timer.start()
+
+func _on_animation_player_animation_finished(anim_name):
+	animationPlayer.seek(0, true)
